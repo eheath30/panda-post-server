cc09f236c3e5d7ec93d0dab067181b018ecce977
eheath30 <elliot.heath@btinternet.com>
2022-07-21 22:30:20 +0100

    testing mongodb

--- dbConfig.js
+++ dbConfig.js
@@ -1,1 +1,10 @@
-const{MongoClient:MongoClient}=require("mongodb"),connectionUrl=process.env.DB_NAME,dbName=process.env.MONGO_NAME,init=async()=>(await MongoClient.connect(connectionUrl)).db(dbName);module.exports={init:init};
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://elliot:<password>@cluster0.fyzximo.mongodb.net/?retryWrites=true&w=majority";
+const init = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+init.connect(err => {
+  const collection = init.db("test").collection("devices");
+  // perform actions on the collection object
+  init.close();
+});
+
+module.exports=init;
